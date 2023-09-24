0415f0043941a5bba03f1de2e37f047b617dcc7f
Donya <thisis2nya@gmail.com>
2023-09-24 23:18:48 +0330

    core: setup database connection

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-// backend server
+const express = require("express");
+const mongoose = require("mongoose");
+
+// app
+const app = express();
+
+// connect database
+const dbURI =
+  "mongodb://root:K4DQ9H8qQZU767tY@services.irn1.chabokan.net:28938/shop?authSource=admin";
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected successfully :)");
+    app.listen(3001);
+  })
+  .catch((err) => {
+    console.log("Database connection error: ", err);
+  });
