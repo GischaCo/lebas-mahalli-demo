6d98aacdb08c218d9455db763f2a5870c6f2ae73
Donya <thisis2nya@gmail.com>
2023-09-26 19:59:51 +0330

    feat: add some options to login form

--- frontend/components/login/LoginForm.vue
+++ frontend/components/login/LoginForm.vue
@@ -22,12 +22,26 @@
       >.
     </p>
 
-    <!-- button -->
-    <button
-      class="w-full h-12 text-white bg-gradient-to-r from-primary to-accent disabled:brightness-75 disabled:cursor-not-allowed rounded-lg hover:shadow-lg transition-all"
-    >
-      ورود
-    </button>
+    <!-- sign-up alert -->
+    <p class="text-xs text-neutral-600">
+      اگه تا حالا حساب کاربری نساختی، رو دکمه‌ی ثبت‌نام کلیک کن.
+    </p>
+
+    <div class="w-full flex items-center justify-between gap-2">
+      <!-- login button -->
+      <button
+        class="w-8/12 h-12 text-white bg-gradient-to-r from-primary to-accent disabled:brightness-75 disabled:cursor-not-allowed rounded-lg hover:shadow-lg transition-all"
+      >
+        ورود
+      </button>
+      <!-- sign-up link -->
+      <nuxt-link
+        to="/auth/sign-up"
+        class="w-4/12 h-12 text-primary flex items-center justify-center border-2 border-primary rounded-lg hover:shadow-lg transition-all"
+      >
+        <span class="text-primary">ثبت‌نام</span>
+      </nuxt-link>
+    </div>
   </form>
 </template>
 
---
+++ frontend/pages/auth/index/sign-up.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="w-full p-6 flex flex-col items-start justify-center gap-3">
+    <nuxt-link to="/" class="flex items-center justify-start gap-1.5">
+      <base-icon
+        name="arrow-right-solid"
+        class="w-3 fill-neutral-600 ml-2"
+      ></base-icon>
+      <base-icon name="house-solid" class="w-3.5 fill-neutral-600"></base-icon>
+      <span class="text-sm text-neutral-600">صفحه اصلی</span>
+    </nuxt-link>
+
+    <h1 class="text-xl font-bold text-primary">ورود به حساب کاربری</h1>
+
+    <login-form></login-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthSignUpPage",
+  layout: "auth",
+};
+</script>
