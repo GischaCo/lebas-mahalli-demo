12582caca2aafc411b76f6d4c23c2234c0a78e61
Donya <thisis2nya@gmail.com>
2023-09-26 06:50:39 +0330

    feat: show header categories arrows on hover

--- frontend/components/header/HeaderNavbarItem.vue
+++ frontend/components/header/HeaderNavbarItem.vue
@@ -17,23 +17,28 @@
     <!-- child categories -->
     <div
       v-if="hasChild(category._id) && isDropdownOpened"
-      class="absolute min-w-max px-8 py-4 bg-white rounded-xl flex flex-col items-start justify-start gap-2 shadow-lg"
+      class="absolute min-w-max pl-8 pr-2 py-4 bg-white rounded-xl flex flex-col items-start justify-start gap-2 shadow-lg"
     >
       <nuxt-link
         :to="`/products${category.path}`"
-        class="flex items-center justify-start gap-2"
+        class="w-full flex items-center justify-start gap-2 group"
       >
         <base-icon
           name="arrow-left-solid"
-          class="w-3 fill-neutral-500"
+          class="w-3 fill-neutral-500 scale-x-0 group-hover:scale-100 transition-all origin-right"
         ></base-icon>
         <span class="text-neutral-500">همه</span>
       </nuxt-link>
       <nuxt-link
+        class="w-full flex items-center justify-start gap-2 group"
         :to="`/products${childCategory.path}`"
         :key="childCategory._id"
         v-for="childCategory in childCategoriesById(category._id)"
       >
+        <base-icon
+          name="arrow-left-solid"
+          class="w-3 fill-neutral-500 scale-x-0 group-hover:scale-100 transition-all origin-right"
+        ></base-icon>
         <span class="text-neutral-500">{{ childCategory.title }}</span>
       </nuxt-link>
     </div>
