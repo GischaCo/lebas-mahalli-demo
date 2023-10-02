6b28384d53bf5c2a22d6a77cdf894ebb4b47caa2
Donya <thisis2nya@gmail.com>
2023-10-02 22:17:47 +0330

    feat: create add-product request controller

--- backend/controlllers/product.controller.js
+++ backend/controlllers/product.controller.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 const Product = require("../models/product.model");
+const fs = require("fs");
 // const ObjectId = require("mongodb").ObjectId;
 
 const addProduct = async (req, res) => {
@@ -9,9 +10,6 @@ const addProduct = async (req, res) => {
     // get token from request header
     const userToken = req.header("Authorization");
     const tokenArray = userToken.split(" ");
-
-    // new product data coming from request's body
-    const productData = req.body;
 
     // get & decode user token data
     const decodedData = jwt.verify(tokenArray[1], process.env.TOKEN_KEY);
@@ -28,14 +26,40 @@ const addProduct = async (req, res) => {
       });
     }
 
+    // work on product's binary image
+    const image = req.body.image;
+    const bufferedImage = new Buffer(image, "base64");
+
+    // work on product's binary images
+    const images = req.body.images;
+    let bufferedImages = [];
+    for (let i in images) {
+      const bufferedItem = new Buffer(images[i], "base64");
+      bufferedImages.push(bufferedItem);
+    }
+
+    // set default data
+    const defaultData = {
+      sales: 0,
+      comments: [],
+    };
+
+    // get product's data coming from request's body
+    const reqBody = req.body;
+
+    // set product's final data
+    const productData = {
+      ...reqBody,
+      image: bufferedImage,
+      images: bufferedImages,
+      ...defaultData,
+    };
+
     // add product to database
-    Product.create({
-      ...productData,
-    });
+    Product.create(productData);
 
     // set response
     res.status(200).send({
-      data: result,
       message: "محصول با موفقیت ثبت شد",
       status: 200,
       success: true,
@@ -44,7 +68,7 @@ const addProduct = async (req, res) => {
     res.status(400).send({
       message: "خطا در ثبت محصول",
       status: 400,
-      success: true,
+      success: false,
     });
     console.log(err);
   }
