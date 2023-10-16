1f26790a97831bef78e354ae2b798bc482b9a3f9
Donya <thisis2nya@gmail.com>
2023-10-16 17:58:59 +0330

    feat: create forget-password page

---
+++ frontend/pages/auth/index/forget-password.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full h-44 flex flex-col items-center justify-center gap-2">
+    <p>این قابلیت به زودی اضافه خواهد شد</p>
+    <p>
+      در صورت نیاز با پشتیبانی تماس بگیرید:
+      <span class="text-primary font-bold">09021029245</span>
+    </p>
+    <nuxt-link to="/auth/login">
+      <span class="text-sm text-blue-700 underline">بازگشت به صفحه ورود</span>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthForgetPasswordPage",
+};
+</script>
