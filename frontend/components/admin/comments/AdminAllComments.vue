c739af2d690c85d4f9c8e43987d1269226a8c9fd
Donya <thisis2nya@gmail.com>
2023-10-31 21:35:29 +0330

    refactor: re-design comments list (admin)

--- frontend/components/admin/comments/AdminAllComments.vue
+++ frontend/components/admin/comments/AdminAllComments.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="w-full">
     <!-- comments data table -->
-    <table
+    <!-- <table
       v-if="comments !== null && comments.length"
       class="w-full bg-slate-50 rounded-xl overflow-hidden shadow-md"
     >
@@ -13,7 +13,7 @@
           <td><p class="text-sm md:text-base">محصول</p></td>
           <td><p class="text-sm md:text-base">متن</p></td>
           <td><p class="text-sm md:text-base">تاریخ</p></td>
-          <td><p class="text-sm md:text-base">لینک</p></td>
+          <td><p class="text-sm md:text-base">حذف</p></td>
         </tr>
       </thead>
       <tbody class="w-full">
@@ -41,23 +41,94 @@
             </p>
           </td>
           <td>
-            <p class="text-overflow text-sm md:text-base">
-              {{ comment.text }}
-            </p>
+            <nuxt-link :to="`/products/${comment.product.id}`">
+              <p class="text-overflow text-sm md:text-base text-primary">
+                {{ comment.text }}
+              </p>
+            </nuxt-link>
           </td>
           <td>
-            <p class="text-sm md:text-base">
+            <p class="text-xs md:text-base">
               {{ $moment(comment.createdAt).format("jYYYY/jM/jDD") }}
             </p>
           </td>
           <td>
-            <nuxt-link :to="`/products/${comment.product.id}`">
-              <span class="text-sm text-primary">مشاهده</span>
-            </nuxt-link>
+            <button
+              title="حذف"
+              @click="deleteComment(comment.product.id)"
+              class="p-1.5 md:p-2 rounded bg-slate-700 shadow-md hover:shadow-lg transition-all"
+            >
+              <base-icon
+                class="w-3 h-3 md:w-3.5 md:h-3.5 fill-white"
+                name="trash-can-regular"
+              ></base-icon>
+            </button>
           </td>
         </tr>
       </tbody>
-    </table>
+    </table> -->
+
+    <div
+      v-if="comments !== null && comments.length"
+      class="w-full flex flex-col items-center justify-start gap-2"
+    >
+      <div v-for="comment in comments" :key="comment._id" class="w-full">
+        <nuxt-link
+          :to="`/products/${comment.product.id}`"
+          class="w-full p-3 bg-slate-50 flex flex-col items-start justify-start gap-2 rounded-xl shadow hover:shadow-none transition-all"
+        >
+          <!-- header -->
+          <div class="w-full flex items-center justify-start gap-3">
+            <h3 class="text-base text-primary font-bold">
+              {{ comment.user.fullname }}
+            </h3>
+            <p class="text-xs md:text-sm text-slate-400">
+              آیدی: {{ comment.user.id }}
+            </p>
+          </div>
+
+          <!-- divider -->
+          <span class="w-full h-[1px] rounded-[50%] bg-slate-100"></span>
+
+          <!-- product -->
+          <div class="w-full flex items-center justify-start gap-2">
+            <p class="text-sm text-primary font-bold">محصول</p>
+
+            <base-icon
+              class="w-3 fill-primary"
+              name="arrow-left-solid"
+            ></base-icon>
+
+            <p class="text-sm text-primary font-bold">
+              {{ comment.product.title }}
+            </p>
+          </div>
+
+          <!-- body -->
+          <p class="text-overflow w-full text-sm text-slate-500">
+            {{ comment.text }}
+          </p>
+
+          <!-- divider -->
+          <span class="w-full h-[1px] rounded-[50%] bg-slate-100"></span>
+
+          <!-- delete -->
+          <button
+            title="حذف محصول"
+            @click.prevent="deleteComment(comment._id)"
+            class="px-2.5 py-1 mt-1 bg-none hover:bg-red-400 flex items-center justify-center gap-2 rounded border-2 border-red-400 transition-all group"
+          >
+            <span class="text-sm text-red-400 group-hover:text-slate-50"
+              >حذف</span
+            >
+            <base-icon
+              class="w-3.5 h-3.5 fill-red-400 group-hover:fill-slate-50"
+              name="trash-can-regular"
+            ></base-icon>
+          </button>
+        </nuxt-link>
+      </div>
+    </div>
 
     <!-- empty state -->
     <p
@@ -95,6 +166,9 @@ const comments = computed(() => {
 const getComments = () => {
   store.dispatch("admin/getComments");
 };
+const deleteComment = (x) => {
+  console.log("Deleted::::", x);
+};
 
 // lifecycles
 onMounted(() => {
@@ -110,8 +184,8 @@ td {
 .text-overflow {
   overflow: hidden;
   display: -webkit-box !important;
-  -webkit-line-clamp: 2 !important;
-  line-clamp: 2 !important;
+  -webkit-line-clamp: 6 !important;
+  line-clamp: 6 !important;
   -webkit-box-orient: vertical !important;
 }
 </style>
